352b5b402d2e39e65e5b4773992a2735595ccdb4
Artemiy Garin <kvirair@gmail.com>
2013-10-16 19:29:18 +0400

    - add zbar libraries, camera surface

---
+++ src/garin/artemiy/simplescanner/library/views/SimpleCameraView.java
@@ -0,0 +1,16 @@
+package garin.artemiy.simplescanner.library.views;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Author: Artemiy Garin
+ * Date: 16.10.13
+ */
+public class SimpleCameraView extends SurfaceView {
+
+    public SimpleCameraView(Context context) {
+        super(context);
+    }
+
+}
