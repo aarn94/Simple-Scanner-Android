aae422fd0e81186738fac95d04ec070534a5253c
Artemiy Garin <kvirair@gmail.com>
2013-10-16 19:23:58 +0400

    - Init commit

---
+++ src/garin/artemiy/simplescanner/example/MainActivity.java
@@ -0,0 +1,15 @@
+package garin.artemiy.simplescanner.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import garin.artemiy.simplescanner.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+}
